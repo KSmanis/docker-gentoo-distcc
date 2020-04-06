5a56f225ebfe9e4215efb2bde35b09fcd146864d
Konstantinos Smanis <konstantinos.smanis@gmail.com>
2020-04-06 09:29:28 +0200

    Apply proper shell quoting

--- entrypoint-distcc-ssh.sh
+++ entrypoint-distcc-ssh.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Create user and set up SSH access
-useradd ${USER}
-echo "${AUTHORIZED_KEYS}" > /home/${USER}/.ssh/authorized_keys
-chown ${USER}:${USER} /home/${USER}/.ssh/authorized_keys
-chmod 600 /home/${USER}/.ssh/authorized_keys
+useradd "${USER}"
+echo "${AUTHORIZED_KEYS}" > "/home/${USER}/.ssh/authorized_keys"
+chown "${USER}:${USER}" "/home/${USER}/.ssh/authorized_keys"
+chmod 600 "/home/${USER}/.ssh/authorized_keys"
 
 # Create missing SSH host keys and execute sshd
 ssh-keygen -A
