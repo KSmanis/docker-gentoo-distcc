c415ebfac160c6536f7efebc88e92b553a897d94
Konstantinos Smanis <konstantinos.smanis@gmail.com>
2020-12-29 11:03:28 +0200

    Allow sshd invocation from Docker command line

--- docker-entrypoint-ssh.sh
+++ docker-entrypoint-ssh.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 set -eu
 
-# Execute sshd in any of the following cases:
+# Execute sshd with some default arguments in any of the following cases:
 # * if no arguments were passed to the Docker command line
 # * if hyphenated flag arguments (e.g., '-f', '-foo', or '--foo') were
 #   passed to the Docker command line
 if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
+    set -- sshd -D -e "$@"
+fi
+
+if [ "$1" = "sshd" ]; then
     # Create user and set up SSH access
     id "${SSH_USERNAME}" >/dev/null 2>&1 || useradd "${SSH_USERNAME}"
     echo "${AUTHORIZED_KEYS}" > "/home/${SSH_USERNAME}/.ssh/authorized_keys"
@@ -13,8 +17,10 @@ if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
     chmod 600 "/home/${SSH_USERNAME}/.ssh/authorized_keys"
     # Create missing SSH host keys
     ssh-keygen -A
-    # Execute sshd
-    exec /usr/sbin/sshd -D -e "$@"
+    # Execute sshd using absolute path
+    shift
+    exec /usr/sbin/sshd "$@"
 fi
 
+# Execute whatever was passed to the Docker command line
 exec "$@"
