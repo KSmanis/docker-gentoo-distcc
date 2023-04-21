f70675d22e23505d74fe2edb7aafd3cf04c64dfc
Konstantinos Smanis <konstantinos.smanis@gmail.com>
2023-04-21 15:59:31 +0300

    refactor: simplify manifest creation

--- build-manifest.sh
+++ build-manifest.sh
@@ -3,14 +3,9 @@ set -eux
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-set --
+manifest="${REPO}:${TAG}"
 for arch in "386" "amd64" "armv6" "armv7" "arm64" "ppc64le"; do
     image="${REPO}:${arch}-${TAG}"
-    if docker manifest inspect "${image}" 1>/dev/null 2>&1; then
-        set -- "$@" "${image}"
-    fi
+    docker manifest create --amend "${manifest}" "${image}" || true
 done
-
-manifest="${REPO}:${TAG}"
-docker manifest create "${manifest}" "$@"
 docker manifest push "${manifest}"
